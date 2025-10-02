99db92ca17854bf9d6c34ffb86381b7770466968
MinecraftModder <modder@example.com>
2025-10-03 00:17:31 +0200

    Add debug logging to GenderCountConverter

--- grzyClothTool/Converters/GenderCountConverter.cs
+++ grzyClothTool/Converters/GenderCountConverter.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Windows.Data;
+using System.Diagnostics;
 
 namespace grzyClothTool.Converters
 {
@@ -15,19 +16,33 @@ namespace grzyClothTool.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // Handle both IEnumerable<object> and ReadOnlyObservableCollection from CollectionViewGroup
-            if (value is IEnumerable items)
+            try
             {
-                var drawables = items.OfType<GDrawable>().ToList();
+                Debug.WriteLine($"GenderCountConverter - Input type: {value?.GetType().Name ?? "null"}");
                 
-                // Count males (Sex = male = 1) and females (Sex = female = 0)
-                int maleCount = drawables.Count(d => d.Sex == Enums.SexType.male);
-                int femaleCount = drawables.Count(d => d.Sex == Enums.SexType.female);
+                // Handle both IEnumerable<object> and ReadOnlyObservableCollection from CollectionViewGroup
+                if (value is IEnumerable items)
+                {
+                    var drawables = items.OfType<GDrawable>().ToList();
+                    Debug.WriteLine($"GenderCountConverter - Found {drawables.Count} drawables");
+                    
+                    // Count males (Sex = male = 1) and females (Sex = female = 0)
+                    int maleCount = drawables.Count(d => d.Sex == Enums.SexType.male);
+                    int femaleCount = drawables.Count(d => d.Sex == Enums.SexType.female);
+                    
+                    Debug.WriteLine($"GenderCountConverter - Males: {maleCount}, Females: {femaleCount}");
+                    
+                    return $"(M: {maleCount} / F: {femaleCount})";
+                }
                 
-                return $"(M: {maleCount} / F: {femaleCount})";
+                Debug.WriteLine("GenderCountConverter - Value is not IEnumerable");
+                return "(M: 0 / F: 0)";
             }
-            
-            return "(M: 0 / F: 0)";
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"GenderCountConverter - Exception: {ex.Message}");
+                return "(Error)";
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
